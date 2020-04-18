67024f539b0b5fb998376503696c82b6486d51a9
BradOdera <bradodera@gmail.com>
2020-04-18 22:09:24 +0300

    finish blog app creation using wordpress

---
+++ footer.php
@@ -0,0 +1,34 @@
+<?php wp_footer(); ?>
+<!--Footer-->
+<footer class="page-footer text-center font-small mt-4 wow fadeIn">
+
+<!--Call to action-->
+<div class="pt-4">
+    <a class="btn btn-outline-white" href="https://t.co/4YncguAi65?amp=1" target="_blank" role="button">Coming Soon
+        <i class="fa fa-download ml-2"></i>
+    </a>
+</div>
+<!--/.Call to action-->
+
+<hr class="my-4">
+
+<!-- Social icons -->
+<div class="pb-4">
+    <a href="https://twitter.com/Biasharaspace" target="_blank">
+        <i class="fab fa-twitter mr-3"></i>
+    </a>
+</div>
+<!-- Social icons -->
+
+<!--Copyright-->
+<div class="footer-copyright py-3">
+    © 2020 Copyright:
+    <a href="https://t.co/4YncguAi65?amp=1" target="_blank">Biashara Space</a>
+</div>
+<!--/.Copyright-->
+</body>
+<script>
+$("#mdb-navigation > ul > li").addClass("page-item")
+$("#mdb-navigation > ul > li > a").addClass("page-link")
+</script>
+</html>
